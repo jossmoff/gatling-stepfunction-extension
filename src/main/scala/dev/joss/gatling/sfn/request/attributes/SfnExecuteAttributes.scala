69ad4e18ec863b20213fb1d5b2537909bf56e824
jossmoff <josshmoffatt@gmail.com>
2023-07-30 13:45:24 +0100

    :boom: Adding `maxExecutionTime` to `SfnExecuteAttributes`

--- src/main/scala/dev/joss/gatling/sfn/request/attributes/SfnExecuteAttributes.scala
+++ src/main/scala/dev/joss/gatling/sfn/request/attributes/SfnExecuteAttributes.scala
@@ -1,8 +1,11 @@
 package dev.joss.gatling.sfn.request.attributes
 
 import io.gatling.core.session.Expression
+
+import scala.concurrent.duration.FiniteDuration
 case class SfnExecuteAttributes(
     requestName: Expression[String],
     stateMachineArn: Expression[String],
-    input: Expression[String]
+    input: Expression[String],
+    maxExecutionTime: Expression[FiniteDuration]
 )
